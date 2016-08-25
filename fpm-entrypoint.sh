cc499bebc6c703b539ef151b60284af88fb08a4e
Maksim Podlesnyi <mpodlesnyi@smartling.com>
2016-08-25 17:50:46 +0300

    pinned to mpidlisnyi/phabricator-base

--- fpm-entrypoint.sh
+++ fpm-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /phabricator_configs_init.sh
 erb -T - /php-fpm.conf.erb > /usr/local/etc/php-fpm.conf
-bin/storage upgrade --force 2>&1 >/dev/null
+#bin/storage upgrade --force
 exec ${@}
