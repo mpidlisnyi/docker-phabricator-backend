0ea9949d11ca38ceb1dc669e0635f43fe63e93c2
Maksim Podlesnyi <mpodlesnyi@smartling.com>
2016-08-26 16:23:43 +0300

    add repository.default-local-path

--- fpm-entrypoint.sh
+++ fpm-entrypoint.sh
@@ -2,4 +2,5 @@
 source /phabricator_configs_init.sh
 erb -T - /php-fpm.conf.erb > /usr/local/etc/php-fpm.conf
 erb -T - /msmtp.conf.erb > /etc/msmtp.conf
+mkdir -p "${REPOSITORY_LOCAL_PATH}"
 exec ${@}
